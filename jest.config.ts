fb799fcc27f100367dbcdbc04210c0b9d4626c40
Satont <satontworldwide@gmail.com>
2021-10-11 19:01:05 +0300

    Add deno tests to jest ignore

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,7 @@ import type { Config } from '@jest/types';
 const config: Config.InitialOptions = {
   testEnvironment: 'node',
   preset: 'ts-jest',
-  testPathIgnorePatterns: ['node_modules', '__tests__/node/helpers'],
+  testPathIgnorePatterns: ['node_modules', '__tests__/node/helpers', '__tests__/deno'],
   globals: {
     'ts-jest': {
       diagnostics: false,
