6c93d84d62b9e81483e2b7fa8a5f4945697fec62
Satont <satontworldwide@gmail.com>
2021-10-11 19:29:54 +0300

    Remove redutant test connection

--- __tests__/deno/helpers/mongo.ts
+++ __tests__/deno/helpers/mongo.ts
@@ -1,15 +1,5 @@
 import { MongoClient, Collection } from "https://deno.land/x/mongo@v0.26.0/mod.ts";
 
-const client = new MongoClient()
-await client.connect({
-  credential: {
-    username: 'test',
-    password: 'test',
-  },
-  servers: [{ host: 'localhost', port: 27017 }],
-  db: 'testdb'
-})
-
 export const createMongoClient = async () => {
   const client = new MongoClient()
   await client.connect({
